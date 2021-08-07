d3f06990377e9597589927e37c79267454b29d55
KaweeX <88151250+Kaweeshachamodx@users.noreply.github.com>
2021-08-07 13:07:10 +0530

    Update clear.js

--- plugins/clear.js
+++ plugins/clear.js
@@ -25,5 +25,5 @@ Asena.addCommand({pattern: 'clear', fromMe: true, desc: END, dontAddCommandList:
 
     await message.sendMessage('```cleaning chat...```');
     await message.client.modifyChat (message.jid, ChatModification.delete);
-    await message.sendMessage('```🏳 Chat cleared 🏳```');
+    await message.sendMessage('*ᴄʜᴀᴛ ᴄʟᴇᴀʀᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ✅*');
 }));
